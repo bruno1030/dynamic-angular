5b6c0ef376aa6f6838bcc1cc6d8a540602c82ae9
Bruno <brunomartins1030@gmail.com>
2025-01-19 11:28:03 +0000

    creates addToCart method

--- dynamic/src/app/services/cart/cart.service.ts
+++ dynamic/src/app/services/cart/cart.service.ts
@@ -6,4 +6,32 @@ import { Injectable } from '@angular/core';
 export class CartService {
 
   constructor() { }
+
+  private cart: any[] = []; // array to store cart items
+
+  // get cart items
+  getCart(){
+    return this.cart;
+  }
+
+  // add item to cart
+  addToCart(product: any, quantity: number){
+    // verify if the item is already in the cart
+    const itemExists = this.cart.some(item => item.product.id === product.id);
+    
+    if (itemExists) {
+      // update the quantity of an existing item
+      for (let item of this.cart) {
+        if (item.product.id === product.id) {
+          item.quantity += quantity;
+          break; // stop the loop, since the item was already found
+        }
+      }
+    } else {
+      // add a new item to the cart
+      this.cart.push({ product, quantity });
+    }
+
+  }
+
 }
