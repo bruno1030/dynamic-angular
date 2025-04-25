273064ba15ee2ba9569e49b93f2469076047ab4d
Bruno <brunomartins1030@gmail.com>
2025-04-25 15:58:01 +0100

    Add login service to be used by login component

--- dynamic/src/app/login/login.component.ts
+++ dynamic/src/app/login/login.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { LoginService } from '../services/login/login.service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  constructor(private loginService: LoginService){
+
+  }
+
   username: string = '';
   password: string = '';
   errorMessage: string = '';
 
   onLogin() {
+
+    var loginIsValid = this.loginService.validateLogin(this.username, this.password);
+
     if (this.username === 'admin' && this.password === 'password123') {
       this.errorMessage = '';
       alert('Login successful!');
